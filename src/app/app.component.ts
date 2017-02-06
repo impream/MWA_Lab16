9f633f0ff9d23f5ba0fb7c40f870095c7e185735
prem nakarmi <prem nakarmi>
2017-02-05 18:01:27 -0600

    adding files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
+import {FormGroup, FormControl, Validators, FormBuilder} from '@angular/forms'
+import { Observable } from "rxjs/Rx";
 
+import { UserDataService } from './user-data.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +10,30 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app works!';
-}
+  userpost:FormGroup;
+  constructor(fb:FormBuilder, private userSrvc:UserDataService){
+    this.userpost = fb.group({
+      'name':['',Validators.required],
+      'email':['',Validators.required],
+      'post':['',[Validators.required, Validators.minLength(10)]]
+    });
+  }
+
+  onSubmit(form){
+    console.log(form.value);
+  }
+
+  getData() {
+  	console.log('getting data');
+
+  	Observable.forkJoin([this.userSrvc.getUserData(), this.userSrvc.getUserPost()]).subscribe(results => {
+  		console.log(results);
+  		console.log(this.userpost);
+  		let user = {
+	  		name: results[0].name,
+	  		email: results[0].email,
+	  		post: results[1][0].body
+  		};
+  		this.userpost.patchValue(user);
+  	});
+}}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { UserDataService } from './user-data.service';
 
 @NgModule({
   declarations: [
@@ -12,9 +14,10 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    ReactiveFormsModule
   ],
-  providers: [],
+  providers: [UserDataService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
